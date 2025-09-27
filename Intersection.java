d0ca6730babf4b0c65329560776a747ec11adf2f
Kunj Maheshwari <kunjm@outlook.com>
2025-09-27 22:31:27 +0530

    Intersection in Y Shaped Lists

---
+++ Intersection.java
@@ -0,0 +1,23 @@
+class Solution {
+    public Node solve(Node head1, Node head2) {
+        Node temp1 = head1;
+        Node temp2 = head2;
+
+        while (temp1 != temp2) {
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+
+            if (temp1 == temp2) {
+                return temp1;
+            }
+
+            if (temp1 == null) {
+                temp1 = head2;
+            }
+            if (temp2 == null) {
+                temp2 = head1;
+            }
+        }
+        return temp1;
+    }
+}
